074829fd86083f6c8e65ef6c3f260e72c6ea4793
marbenz2 <benzinger.marcel@gmail.com>
2024-09-04 19:44:41 +0200

    refactor: Refactor TripsPage component to use Supabase queries and remove unnecessary code

--- src/app/(app)/protected/trips/page.tsx
+++ src/app/(app)/protected/trips/page.tsx
@@ -1,9 +1,41 @@
-import Trips from "@/components/trips/Trips";
+import { createClient } from '@/utils/supabase/server'
+import { getGroupTrips, getSubscribedTrips, getUser, getUserGroups } from '@/utils/supabase/queries'
+import Trips from '@/components/Trips'
+import { Tables } from 'database.types'
 
-export default async function ProtectedPage() {
+type Trips = Tables<'trips'>
+type TripMembers = Tables<'trip_members'>
+type Groups = Tables<'groups'>
+type GroupMembers = Tables<'group_members'>
+type SubscribedTrips = {
+  trips: Trips
+  subscribed_at: TripMembers['subscribed_at']
+}[]
+type UserGroups = {
+  group_id: GroupMembers['group_id']
+  favourite: GroupMembers['favourite']
+  groups: Groups
+}[]
+
+export default async function TripsPage() {
+  const supabase = createClient()
+  const [user] = await Promise.all([getUser(supabase)])
+  const [userGroups, subscribedTrips] = await Promise.all([
+    user
+      ? getUserGroups(supabase, user.id).then((groups) => groups as unknown as UserGroups)
+      : null,
+    user
+      ? getSubscribedTrips(supabase, user.id).then((trips) => trips as unknown as SubscribedTrips)
+      : null,
+  ])
+  const [groupTrips] = await Promise.all([userGroups && getGroupTrips(supabase, userGroups)])
+
   return (
-    <section className="max-w-7xl w-full">
-      <Trips />
-    </section>
-  );
+    <Trips
+      user={user}
+      userGroups={userGroups}
+      groupTrips={groupTrips}
+      subscribedTrips={subscribedTrips}
+    />
+  )
 }
