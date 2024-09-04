538aa32d7f9f4f08dc0fa417936ee90c62d2c9d9
marbenz2 <benzinger.marcel@gmail.com>
2024-09-04 19:44:22 +0200

    refactor: Update Home component to use Supabase queries and remove unnecessary code

--- src/app/(app)/protected/page.tsx
+++ src/app/(app)/protected/page.tsx
@@ -1,105 +1,36 @@
-'use client'
+import Dashboard from '@/components/Dashboard'
+import { getSubscribedTrips, getUser, getUserGroups } from '@/utils/supabase/queries'
+import { createClient } from '@/utils/supabase/server'
+import { Tables } from 'database.types'
 
-import { useEffect } from 'react'
-import { useUserStore, useGroupStore, useTripStore } from '../../../stores/store'
-import { createClient } from '@/utils/supabase/client'
-import { redirect } from 'next/navigation'
+type Trips = Tables<'trips'>
+type TripMembers = Tables<'trip_members'>
+type Groups = Tables<'groups'>
+type GroupMembers = Tables<'group_members'>
+type SubscribedTrips = {
+  trips: Trips
+  subscribed_at: TripMembers['subscribed_at']
+}[]
+type UserGroups = {
+  group_id: GroupMembers['group_id']
+  favourite: GroupMembers['favourite']
+  groups: Groups
+}[]
 
-const fetchInitialData = async () => {
+export default async function Home() {
   const supabase = createClient()
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
+  const user = await getUser(supabase)
 
-  if (!user) {
-    return redirect('/login')
-  }
+  // Fetch user groups and subscribed trips if the user is authenticated
+  const [userGroups, subscribedTrips] = await Promise.all([
+    user
+      ? getUserGroups(supabase, user.id).then((groups) => groups as unknown as UserGroups)
+      : null,
+    user
+      ? getSubscribedTrips(supabase, user.id).then((trips) => trips as unknown as SubscribedTrips)
+      : null,
+  ])
 
-  try {
-    const {
-      data: { user },
-      error: userError,
-    } = await supabase.auth.getUser()
-
-    if (userError) {
-      console.error('User Error:', userError)
-      throw new Error('Error loading user data')
-    }
-
-    ;(useUserStore.getState() as { setUser: (user: any) => void }).setUser(user)
-
-    const { data: groups, error: groupsError } = await supabase
-      .from('group_members')
-      .select('group_id, groups(name, description, created_at)')
-      .eq('user_id', user?.id ?? '')
-
-    if (groupsError) {
-      console.error('Groups Error:', groupsError)
-      throw new Error('Error loading group data')
-    }
-
-    if (!groups) {
-      throw new Error('No groups found')
-    }
-
-    ;(useGroupStore.getState() as { setGroups: (groups: any) => void }).setGroups(
-      groups.map((gm) => gm.groups),
-    )
-
-    const { data: trips, error: tripsError } = await supabase
-      .from('trips')
-      .select('*')
-      .in(
-        'group',
-        groups.map((g) => g.group_id),
-      )
-
-    if (tripsError) {
-      console.error('Trips Error:', tripsError)
-      throw new Error('Error loading trip data')
-    }
-
-    if (!trips) {
-      throw new Error('No trips found')
-    }
-
-    ;(useTripStore.getState() as { setTrips: (trips: any) => void }).setTrips(trips)
-
-    // Weitere Daten laden und in entsprechende Stores setzen, falls nötig
-  } catch (error) {
-    console.error('Error loading data:', error)
-  }
-}
-
-export default function Home() {
-  const user = useUserStore((state) => state.user)
-  const groups = useGroupStore((state) => state.groups)
-  const trips = useTripStore((state) => state.trips)
-
-  useEffect(() => {
-    fetchInitialData()
-  }, [])
-
-  return (
-    <div>
-      <h1>Willkommen zu deiner Reiseplanungs-App!</h1>
-      {/* Weitere UI-Komponenten */}
-      <div>
-        <h1>Dashboard für {user?.email}</h1>
-        <h2>Gruppen</h2>
-        <ul>
-          {groups.map((group, id) => (
-            <li key={id}>{group.name}</li>
-          ))}
-        </ul>
-        <h2>Trips</h2>
-        <ul>
-          {trips.map((trip) => (
-            <li key={trip.id}>{trip.name}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  )
+  return <Dashboard user={user} subscribedTrips={subscribedTrips} userGroups={userGroups} />
 }
