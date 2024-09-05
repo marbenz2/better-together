8b2f303ab2b698c6a24d6d01db57f3d078f8a59c
marbenz2 <benzinger.marcel@gmail.com>
2024-09-05 19:43:03 +0200

    refactor: Update GroupManagement component to include GroupMembers and LeaveGroup functionality

--- src/components/dashboard/Groups/GroupManagement.tsx
+++ src/components/dashboard/Groups/GroupManagement.tsx
@@ -4,31 +4,40 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion'
-
 import { CardDescription } from '@/components/ui/card'
-import type { UserGroupsType } from '@/types/dashboard'
+import type { GroupMembersType, PublicProfilesType, UserGroupsType } from '@/types/dashboard'
 import FavouriteGroups from './FavouriteGroups'
 import GroupCode from './GroupCode'
 import CreateGroup from './CreateGroup'
 import JoinGroup from './JoinGroup'
 import RenameGroup from './RenameGroup'
 import DeleteGroup from './DeleteGroup'
+import LeaveGroup from './LeaveGroup'
+import GroupMembers from './GroupMembers'
 
 interface GroupManagementProps {
+  user: any
+  publicProfiles: PublicProfilesType
+  groupMembers: GroupMembersType
   userGroups: UserGroupsType
-  groupId: string | false | null
+  groupId: string | null
   createGroup: (groupName: string) => Promise<void>
   joinGroup: (groupId: string) => Promise<void>
   deleteGroup: (groupId: string) => Promise<void>
+  leaveGroup: (groupId: string) => Promise<void>
   renameGroup: (groupId: string, newName: string) => Promise<void>
   setFavourite: (groupId: string, favourite: boolean) => Promise<void>
 }
 
 export default function GroupManagement({
+  user,
+  publicProfiles,
+  groupMembers,
   userGroups,
   groupId,
   createGroup,
   joinGroup,
+  leaveGroup,
   deleteGroup,
   renameGroup,
   setFavourite,
@@ -45,12 +54,24 @@ export default function GroupManagement({
             {userGroups && userGroups.length > 0 && (
               <FavouriteGroups userGroups={userGroups} setFavouriteGroup={setFavourite} />
             )}
+            {/* Gruppenmitglieder */}
+            {userGroups && userGroups.length > 0 && (
+              <GroupMembers
+                publicProfiles={publicProfiles}
+                groupMembers={groupMembers}
+                groupId={groupId}
+              />
+            )}
+            {/* Gruppencode */}
             {userGroups && userGroups.length > 0 && (
               <GroupCode userGroups={userGroups} groupId={groupId} />
             )}
             <div className="flex flex-col gap-12 w-full max-w-lg lg:max-w-full lg:flex-row">
-              <JoinGroup joinGroup={joinGroup} />
+              {/* Gruppen erstellen */}
               <CreateGroup createGroup={createGroup} />
+              {/* Gruppen beitreten */}
+              <JoinGroup joinGroup={joinGroup} />
+              {/* Gruppen umbenennen */}
               {userGroups &&
                 groupId &&
                 userGroups.find(
@@ -63,6 +84,17 @@ export default function GroupManagement({
                   />
                 )}
             </div>
+            {/* Gruppen verlassen */}
+            {userGroups && groupId && (
+              <LeaveGroup
+                user={user}
+                leaveGroup={leaveGroup}
+                groupId={groupId}
+                userGroups={userGroups}
+                groupMembers={groupMembers}
+              />
+            )}
+            {/* Gruppen löschen */}
             {userGroups &&
               groupId &&
               userGroups.find((group) => group.group_id === groupId && group.role === 'admin') && (
