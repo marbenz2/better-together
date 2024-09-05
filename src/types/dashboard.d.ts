8e2e621d863e82ec1583e5400cbc349eadccbfda
marbenz2 <benzinger.marcel@gmail.com>
2024-09-05 19:41:34 +0200

    refactor: Add GroupMembersType to dashboard types and PublicProfilesType

--- src/types/dashboard.d.ts
+++ src/types/dashboard.d.ts
@@ -4,6 +4,12 @@ type Trips = Tables<'trips'>
 type TripMembers = Tables<'trip_members'>
 type Groups = Tables<'groups'>
 type GroupMembers = Tables<'group_members'>
+type Profiles = Tables<'profiles'>
+
+export type GroupMembersType = {
+  user_id: GroupMembers['user_id']
+  role: GroupMembers['role']
+}[]
 
 export type UserGroupsType = {
   group_id: GroupMembers['group_id']
@@ -16,3 +22,5 @@ export type SubscribedTripsType = {
   trips: Trips
   subscribed_at: TripMembers['subscribed_at']
 }[]
+
+export type PublicProfilesType = Profiles[]
