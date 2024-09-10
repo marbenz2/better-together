7c49143ed30c936d164082783a2a9060ff58fd65
marbenz2 <benzinger.marcel@gmail.com>
2024-09-10 13:44:10 +0200

    refactor: Removed unused code and simplified  Groups component

--- src/app/(app)/protected/groups/page.tsx
+++ src/app/(app)/protected/groups/page.tsx
@@ -1,3 +1,23 @@
-export default async function GroupPage() {
-  return <div>page</div>;
+'use client'
+
+import { useEffect } from 'react'
+import Groups from '@/components/groups/Groups'
+import { useToast } from '@/components/ui/use-toast'
+import { useToastStore } from '@/stores/toastStore'
+
+export default function Home() {
+  const { toast } = useToast()
+  const { title, message, variant } = useToastStore()
+
+  useEffect(() => {
+    if (title && message && variant) {
+      toast({
+        title,
+        description: message,
+        variant,
+      })
+    }
+  }, [title, message, variant, toast])
+
+  return <Groups />
 }
