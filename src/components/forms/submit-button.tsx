9f6acdf773821305e613529b1fc7cfd3c3b9130a
marbenz2 <benzinger.marcel@gmail.com>
2024-09-10 15:10:33 +0200

    Refactor SubmitButton component to display a spinner when pending

--- src/components/forms/submit-button.tsx
+++ src/components/forms/submit-button.tsx
@@ -1,25 +1,22 @@
-"use client";
+'use client'
 
-import { useFormStatus } from "react-dom";
-import { type ComponentProps } from "react";
-import { Button } from "../ui/button";
+import { useFormStatus } from 'react-dom'
+import { type ComponentProps } from 'react'
+import { Button } from '../ui/button'
+import Spinner from '@/components/ui/Spinner'
 
-type Props = ComponentProps<"button"> & {
-  pendingText?: string;
-};
+type Props = ComponentProps<'button'> & {
+  pendingText?: string
+}
 
-export function SubmitButton({
-  children,
-  pendingText = "Submitting...",
-  ...props
-}: Props) {
-  const { pending, action } = useFormStatus();
+export function SubmitButton({ children, pendingText = 'Submitting...', ...props }: Props) {
+  const { pending, action } = useFormStatus()
 
-  const isPending = pending && action === props.formAction;
+  const isPending = pending && action === props.formAction
 
   return (
     <Button {...props} type="submit" aria-disabled={pending} ref={undefined}>
-      {isPending ? pendingText : children}
+      {isPending ? <Spinner /> : children}
     </Button>
-  );
+  )
 }
