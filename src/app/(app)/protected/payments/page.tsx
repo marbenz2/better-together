54c1749292ab155b8b14fad709ad55ae13b28b98
marbenz2 <benzinger.marcel@gmail.com>
2024-09-10 13:44:26 +0200

    refactor: Simplify Payments page component and add toast notifications

--- src/app/(app)/protected/payments/page.tsx
+++ src/app/(app)/protected/payments/page.tsx
@@ -1,53 +1,29 @@
-import { createClient } from '@/utils/supabase/server'
-import {
-  getGroupTrips,
-  getPaymentDetails,
-  getSubscribedTrips,
-  getUser,
-  getUserGroups,
-} from '@/utils/supabase/queries'
-import Trips from '@/components/Trips'
+'use client'
+
 import Payments from '@/components/payments/Payments'
-import { Tables } from 'database.types'
+import { useToast } from '@/components/ui/use-toast'
+import { usePaymentStore } from '@/stores/paymentStore'
+import { useToastStore } from '@/stores/toastStore'
+import { useEffect } from 'react'
 
-type Trips = Tables<'trips'>
-type Groups = Tables<'groups'>
-type GroupMembers = Tables<'group_members'>
-type PaymentDetails = {
-  trip_id: string
-  down_payment: number | null
-  full_payment: number | null
-  final_payment: number | null
-  down_payment_paypal_id: string | null
-  full_payment_paypal_id: string | null
-  final_payment_paypal_id: string | null
-}
-type SubscribedTrips = {
-  subscribed_at: string
-  trips: Trips
-}
-type UserGroups = {
-  group_id: GroupMembers['group_id']
-  favourite: GroupMembers['favourite'] // Änderung hier
-  role: 'admin' | 'member'
-  groups: Groups
-}[]
+export default function PaymentPage() {
+  const { getPaymentDetails } = usePaymentStore()
+  const { toast } = useToast()
+  const { title, message, variant } = useToastStore()
 
-export default async function PaymentPage() {
-  const supabase = createClient()
-  const [user] = await Promise.all([getUser(supabase)])
-  const [userGroups, subscribedTrips, paymentDetails] = await Promise.all([
-    user
-      ? getUserGroups(supabase, user.id).then((groups) => groups as unknown as UserGroups[])
-      : [],
-    user
-      ? getSubscribedTrips(supabase, user.id).then((trips) => trips as unknown as SubscribedTrips[])
-      : [],
-    user ? getPaymentDetails(supabase, user.id).then((details) => details as PaymentDetails[]) : [],
-  ])
-  const [groupTrips] = await Promise.all([
-    userGroups.length > 0 ? getGroupTrips(supabase, userGroups as UserGroups) : [],
-  ])
+  useEffect(() => {
+    if (title && message && variant) {
+      toast({
+        title,
+        description: message,
+        variant,
+      })
+    }
+  }, [title, message, variant, toast])
 
-  return <Payments user={user} paymentDetails={paymentDetails} subscribedTrips={subscribedTrips} />
+  useEffect(() => {
+    getPaymentDetails()
+  }, [getPaymentDetails])
+
+  return <Payments />
 }
