1ff7f96a1d77d9c779bd1015c98c4d133ed1d0f4
marbenz2 <benzinger.marcel@gmail.com>
2024-09-10 13:50:16 +0200

    refactor: Add showNotification utility function for displaying notifications

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { useToastStore } from '@/stores/toastStore'
+import { NotificationVariant } from '@/types/notification.'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
@@ -7,4 +9,17 @@ export function cn(...inputs: ClassValue[]) {
 
 export function copyToClipboard(text: string | number | null | undefined) {
   navigator.clipboard.writeText(text as string)
+}
+
+export const showNotification = (
+  title: string,
+  message: string,
+  variant: NotificationVariant,
+): void => {
+  const toastStore = useToastStore.getState()
+  toastStore.clearToast()
+  toastStore.showToast(title, message, variant)
+  setTimeout(() => {
+    toastStore.clearToast()
+  }, 3000)
 }
