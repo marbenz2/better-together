4796ee080ac40c9732bf66c1b38c540081727ebe
marbenz2 <benzinger.marcel@gmail.com>
2024-09-10 13:42:41 +0200

    refactor: Refactor PayPal capture order route to use updated utils and queries

--- src/app/(app)/api/paypal/captureorder/route.js
+++ src/app/(app)/api/paypal/captureorder/route.js
@@ -1,37 +1,37 @@
-import { updatePaymentStatus } from "@/lib/actions";
-import { NextResponse } from "next/server";
+import { updatePaymentStatus } from '@/utils/supabase/queries'
+import { NextResponse } from 'next/server'
 
 export async function POST(req) {
-  const { order_id, payment_type, user_id, trip_id } = await req.json(); // Extrahieren Sie die orderID direkt aus dem JSON-Body
+  const { order_id, payment_type, user_id, trip_id } = await req.json() // Extrahieren Sie die orderID direkt aus dem JSON-Body
 
   if (!order_id) {
     return NextResponse.json(
       {
         success: false,
-        message: "Please provide orderID",
+        message: 'Please provide orderID',
       },
-      { status: 400 }
-    );
+      { status: 400 },
+    )
   }
 
   try {
-    const accessToken = await getAccessToken();
-    const capture = await captureOrder(accessToken, order_id);
+    const accessToken = await getAccessToken()
+    const capture = await captureOrder(accessToken, order_id)
 
     if (!capture || !capture.id) {
       return NextResponse.json(
         {
           success: false,
-          message: "Some error occurred at backend",
+          message: 'Some error occurred at backend',
         },
-        { status: 500 }
-      );
+        { status: 500 },
+      )
     }
 
-    const transaction_id = capture.id;
+    const transaction_id = capture.id
 
     if (payment_type && user_id && trip_id) {
-      await updatePaymentStatus(user_id, trip_id, payment_type, transaction_id);
+      await updatePaymentStatus(user_id, trip_id, payment_type, transaction_id)
     }
 
     return NextResponse.json(
@@ -39,53 +39,50 @@ export async function POST(req) {
         success: true,
         data: { capture },
       },
-      { status: 200 }
-    );
+      { status: 200 },
+    )
   } catch (err) {
-    console.log("Error at Capture Order: ", err);
+    console.log('Error at Capture Order: ', err)
     return NextResponse.json(
       {
         success: false,
-        message: "Could not capture order",
+        message: 'Could not capture order',
       },
-      { status: 500 }
-    );
+      { status: 500 },
+    )
   }
 }
 
 async function getAccessToken() {
-  const clientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;
-  const clientSecret = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_SECRET;
+  const clientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
+  const clientSecret = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_SECRET
 
-  const auth = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+  const auth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
 
-  const response = await fetch(
-    "https://api-m.sandbox.paypal.com/v1/oauth2/token",
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Basic ${auth}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: "grant_type=client_credentials",
-    }
-  );
+  const response = await fetch('https://api-m.sandbox.paypal.com/v1/oauth2/token', {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${auth}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: 'grant_type=client_credentials',
+  })
 
-  const data = await response.json();
-  return data.access_token;
+  const data = await response.json()
+  return data.access_token
 }
 
 async function captureOrder(accessToken, order_id) {
   const response = await fetch(
     `https://api-m.sandbox.paypal.com/v2/checkout/orders/${order_id}/capture`,
     {
-      method: "POST",
+      method: 'POST',
       headers: {
         Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-    }
-  );
+    },
+  )
 
-  return response.json();
+  return response.json()
 }
