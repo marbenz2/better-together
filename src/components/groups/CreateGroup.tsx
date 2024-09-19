1ef04375bff922818516174d7359c50a98c00b9a
marbenz2 <benzinger.marcel@gmail.com>
2024-09-19 19:30:38 +0200

    Refactor CreateGroup component to use ResponsiveDialog for group creation confirmation

--- src/components/groups/CreateGroup.tsx
+++ src/components/groups/CreateGroup.tsx
@@ -7,15 +7,15 @@ import { SubmitButton } from '@/components/forms/submit-button'
 import { CardTitle } from '@/components/ui/card'
 import { PlusIcon } from 'lucide-react'
 import { useGroupStore } from '@/stores/groupStores'
+import { ResponsiveDialog } from '@/components/ResponsiveDialog'
 
 export default function CreateGroup() {
   const [newGroupName, setNewGroupName] = useState('')
   const { createGroup } = useGroupStore()
 
-  const handleCreateGroup = async (formData: FormData) => {
-    const groupName = formData.get('groupIdCreate') as string
+  const handleCreateGroup = async () => {
     try {
-      await createGroup(groupName)
+      await createGroup(newGroupName)
       setNewGroupName('')
     } catch (error) {
       console.error('Fehler beim Erstellen der Gruppe:', error)
@@ -26,8 +26,12 @@ export default function CreateGroup() {
     setNewGroupName(event.target.value)
   }
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }
+
   return (
-    <form className="flex flex-col gap-4 w-full">
+    <form className="flex flex-col gap-4 w-full" onSubmit={handleSubmit}>
       <Label htmlFor="groupIdCreate">
         <CardTitle className="text-xl">Eine neue Gruppe erstellen</CardTitle>
       </Label>
@@ -43,16 +47,23 @@ export default function CreateGroup() {
           onChange={handleGroupCreateInputChange}
           className="w-full max-w-lg"
         />
-        <SubmitButton
-          aria-label="Gruppe erstellen"
-          formAction={handleCreateGroup}
-          pendingText="Erstelle Gruppe..."
-          className="relative flex text-xs pl-10 w-full max-w-lg"
+        <ResponsiveDialog
+          title="Gruppe erstellen"
+          message={`Bist du sicher, dass du die Gruppe "${newGroupName}" erstellen möchtest?`}
+          confirmText="Gruppe erstellen"
+          onConfirm={handleCreateGroup}
           disabled={newGroupName === ''}
         >
-          <span className="xs:inline truncate">Gruppe erstellen</span>
-          <PlusIcon className="absolute left-4 top-1/2 -translate-y-1/2 w-4 h-4" />
-        </SubmitButton>
+          <SubmitButton
+            aria-label="Gruppe erstellen"
+            pendingText="Erstelle Gruppe..."
+            className="relative flex text-xs pl-10 w-full max-w-lg"
+            disabled={newGroupName === ''}
+          >
+            <span className="xs:inline truncate">Gruppe erstellen</span>
+            <PlusIcon className="absolute left-4 top-1/2 -translate-y-1/2 w-4 h-4" />
+          </SubmitButton>
+        </ResponsiveDialog>
       </div>
     </form>
   )
