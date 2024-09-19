8071d8ea2741edc6444e27005508cc87f337c645
marbenz2 <benzinger.marcel@gmail.com>
2024-09-19 18:14:31 +0200

    feature: add new stores for trip management

--- src/stores/tripStores.ts
+++ src/stores/tripStores.ts
@@ -1,8 +1,17 @@
 import { createClient } from '@/utils/supabase/client'
 import { create } from 'zustand'
 import { useUserStore } from './userStore'
-import { getGroupTrips, getTrip } from '@/utils/supabase/queries'
+import {
+  createTrip,
+  deleteTrip,
+  getGroupTrips,
+  getTrip,
+  getUser,
+  updateTrip,
+} from '@/utils/supabase/queries'
 import { GroupTripsType, GroupTripType } from '@/types/trips'
+import { showNotification } from '@/lib/utils'
+import { NotificationMessage } from '@/types/notification.'
 
 interface TripState {
   trip: GroupTripType | null
@@ -11,6 +20,20 @@ interface TripState {
   groupTrips: GroupTripsType | null
   setGroupTrips: (groupTrips: GroupTripsType) => void
   getGroupTrips: (groupId: string) => Promise<void>
+  createTrip: (trip: GroupTripType) => Promise<void>
+  updateTrip: (trip: GroupTripType) => Promise<void>
+  deleteTrip: (tripId: string) => Promise<void>
+}
+
+const handleError = (error: any, defaultTitle: string, defaultMessage: string) => {
+  const errorMessages: { [key: string]: NotificationMessage } = {}
+
+  const errorMessage = errorMessages[error.code] || {
+    title: defaultTitle,
+    message: defaultMessage,
+    variant: 'destructive',
+  }
+  showNotification(errorMessage.title, errorMessage.message, errorMessage.variant)
 }
 
 export const useTripStore = create<TripState>((set) => ({
@@ -48,6 +71,81 @@ export const useTripStore = create<TripState>((set) => ({
     }
     if (data) {
       set({ groupTrips: data as GroupTripsType })
+    }
+  },
+  createTrip: async (trip: Omit<GroupTripType, 'id'>) => {
+    try {
+      const supabase = createClient()
+      const { data: user } = await getUser(supabase)
+      if (!user) {
+        console.error('Kein Benutzer gefunden')
+        return
+      }
+      const { data, error } = await createTrip(supabase, trip as GroupTripType)
+      if (error) throw error
+
+      if (data && !error) {
+        set({ trip: data as GroupTripType })
+        showNotification(
+          'Reise erstellt',
+          `Die Reise "${trip.name}" wurde erfolgreich erstellt.`,
+          'success',
+        )
+      }
+    } catch (error) {
+      handleError(
+        error,
+        'Fehler beim Erstellen der Reise',
+        'Es ist ein unerwarteter Fehler aufgetreten. Bitte versuchen Sie es später erneut.',
+      )
+    }
+  },
+  updateTrip: async (trip: GroupTripType) => {
+    try {
+      const supabase = createClient()
+      const { data: user } = await getUser(supabase)
+      if (!user) {
+        console.error('Kein Benutzer gefunden')
+        return
+      }
+      const { data, error } = await updateTrip(supabase, trip)
+      if (error) throw error
+      if (data && !error) {
+        set({ trip: data as GroupTripType })
+        showNotification(
+          'Reise aktualisiert',
+          `Die Reise "${trip.name}" wurde erfolgreich aktualisiert.`,
+          'success',
+        )
+      }
+    } catch (error) {
+      handleError(
+        error,
+        'Fehler beim Aktualisieren der Reise',
+        'Es ist ein unerwarteter Fehler aufgetreten. Bitte versuchen Sie es später erneut.',
+      )
+    }
+  },
+  deleteTrip: async (tripId: string) => {
+    try {
+      const supabase = createClient()
+      const { data: user } = await getUser(supabase)
+      if (!user) {
+        console.error('Kein Benutzer gefunden')
+        return
+      }
+      const { data, error } = await deleteTrip(supabase, tripId)
+      if (error) throw error
+      if (data && !error) {
+        set({ trip: null })
+        showNotification('Reise gelöscht', 'Die Reise wurde erfolgreich gelöscht.', 'success')
+      }
+    } catch (error) {
+      handleError(
+        error,
+        'Fehler beim Löschen der Reise',
+        'Es ist ein unerwarteter Fehler aufgetreten. Bitte versuchen Sie es später erneut.',
+      )
     }
   },
 }))
