e3f1fc0495e12dbc93e4df5104910e2a80edc7a5
marbenz2 <benzinger.marcel@gmail.com>
2024-09-19 18:10:36 +0200

    Refactor imports and add info card for empty group in TripsPage

--- src/app/(app)/protected/trips/page.tsx
+++ src/app/(app)/protected/trips/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { useEffect } from 'react'
-import Trips from '@/components/Trips'
+import Trips from '@/components/trips/Trips'
 import { useGroupStore } from '@/stores/groupStores'
 import { useTripStore } from '@/stores/tripStores'
 import { useUserStore } from '@/stores/userStore'
 import { useInitializationStore } from '@/stores/initializationStore'
+import { Card, CardDescription, CardHeader } from '@/components/ui/card'
+import InfoCard from '@/components/ui/info-card'
 
 export default function TripsPage() {
   const { groupId } = useGroupStore()
@@ -18,6 +20,15 @@ export default function TripsPage() {
       getGroupTrips(groupId)
     }
   }, [isInitialized, groupId, getGroupTrips, user.id])
+
+  if (!groupId) {
+    return (
+      <InfoCard
+        description="Du musst eine Gruppe erstellen oder einer Gruppe beitreten, um Reisen zu sehen"
+        variant="info"
+      />
+    )
+  }
 
   return <Trips />
 }
