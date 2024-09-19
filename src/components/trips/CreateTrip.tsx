6958f0d0a04399e32bce61b92603b1466d52e4d3
marbenz2 <benzinger.marcel@gmail.com>
2024-09-19 23:50:36 +0200

    Refactor components to improve trip and payment functionality

--- src/components/trips/CreateTrip.tsx
+++ src/components/trips/CreateTrip.tsx
@@ -13,9 +13,13 @@ import { GroupTripType } from '@/types/trips'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React, { useCallback, useEffect, useState } from 'react'
+import { BackButtonClient } from '../ui/back-button-client'
 
 type TripDataState = {
-  [K in keyof Omit<GroupTripType, 'id'>]: K extends 'beds' | 'rooms' | 'street_number'
+  [K in keyof Omit<GroupTripType, 'id' | 'available_spots' | 'max_spots'>]: K extends
+    | 'beds'
+    | 'rooms'
+    | 'street_number'
     ? number | ''
     : GroupTripType[K]
 }
@@ -155,220 +159,280 @@ export default function CreateTrip() {
   }
 
   return (
-    <form className="flex flex-col gap-4 w-full" onSubmit={handleSubmit}>
-      <Label>
-        <CardTitle className="text-xl">Eine neue Reise erstellen</CardTitle>
-      </Label>
-      <div className="flex flex-col gap-4 w-full max-w-lg">
-        <div className="flex flex-col gap-2 w-full">
-          <RequiredLabel htmlFor="image">Bild der Reise</RequiredLabel>
+    <div className="flex flex-col gap-4 w-full max-w-7xl mx-auto">
+      <BackButtonClient className="static" />
+      <form className="flex flex-col gap-4 w-full items-center" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-4 w-full max-w-lg">
+          <Label>
+            <CardTitle className="text-xl">Eine neue Reise erstellen</CardTitle>
+          </Label>
+          <div className="flex flex-col gap-2 w-full">
+            <RequiredLabel htmlFor="image">Bild der Reise</RequiredLabel>
+            <Input
+              type="file"
+              id="image"
+              name="image"
+              accept="image/*"
+              required
+              onChange={handleImageUpload}
+              className="w-full"
+            />
+            {tripData.image ? (
+              <div className="relative flex w-full h-64">
+                <Image
+                  src={tripData.image}
+                  alt="Vorschau des hochgeladenen Bildes"
+                  fill
+                  className="object-contain"
+                  loading="lazy"
+                />
+              </div>
+            ) : (
+              <div className="relative flex w-full h-64 items-center justify-center overflow-hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="100%"
+                  height="100%"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                >
+                  <polygon points="0,0 100,0 0,100" fill="rgba(255,255,255,0.2)" />
+                  <polygon points="100,0 100,100 0,100" fill="rgba(255,255,255,0.3)" />
+                </svg>
+              </div>
+            )}
+          </div>
+          <RequiredLabel htmlFor="name">Reisename</RequiredLabel>
           <Input
-            type="file"
-            id="image"
-            name="image"
-            accept="image/*"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Tripname eingeben"
+            autoComplete="off"
             required
-            onChange={handleImageUpload}
+            value={tripData.name}
+            onChange={handleInputChange}
             className="w-full"
           />
-          {tripData.image && (
-            <div className="relative flex w-full h-64">
-              <Image
-                src={tripData.image}
-                alt="Vorschau des hochgeladenen Bildes"
-                fill
-                className="object-contain"
-                loading="lazy"
+          <div className="flex gap-2 w-full">
+            <div className="flex flex-col gap-2 w-full">
+              <RequiredLabel htmlFor="date_from">Startdatum</RequiredLabel>
+              <Input
+                type="date"
+                id="date_from"
+                name="date_from"
+                placeholder="Startdatum eingeben"
+                autoComplete="off"
+                required
+                value={tripData.date_from}
+                onChange={handleInputChange}
+                className="w-full"
               />
             </div>
-          )}
-        </div>
-        <RequiredLabel htmlFor="name">Reisename</RequiredLabel>
-        <Input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Tripname eingeben"
-          autoComplete="off"
-          required
-          value={tripData.name}
-          onChange={handleInputChange}
-          className="w-full"
-        />
-        <div className="flex gap-2 w-full">
+            <div className="flex flex-col gap-2 w-full">
+              <RequiredLabel htmlFor="date_to">Enddatum</RequiredLabel>
+              <Input
+                type="date"
+                id="date_to"
+                name="date_to"
+                placeholder="Enddatum eingeben"
+                autoComplete="off"
+                required
+                value={tripData.date_to}
+                onChange={handleInputChange}
+                className="w-full"
+              />
+            </div>
+          </div>
           <div className="flex flex-col gap-2 w-full">
-            <RequiredLabel htmlFor="date_from">Startdatum</RequiredLabel>
+            <RequiredLabel htmlFor="land">Land</RequiredLabel>
             <Input
-              type="date"
-              id="date_from"
-              name="date_from"
-              placeholder="Startdatum eingeben"
+              type="text"
+              id="land"
+              name="land"
+              placeholder="Land eingeben"
               autoComplete="off"
               required
-              value={tripData.date_from}
+              value={tripData.land}
+              onChange={handleInputChange}
+              className="w-full"
+            />
+            <Label htmlFor="area">Gebiet</Label>
+            <Input
+              type="text"
+              id="area"
+              name="area"
+              placeholder="Gebiet eingeben"
+              autoComplete="off"
+              value={tripData.area || ''}
               onChange={handleInputChange}
               className="w-full"
             />
           </div>
-          <div className="flex flex-col gap-2 w-full">
-            <RequiredLabel htmlFor="date_to">Enddatum</RequiredLabel>
-            <Input
-              type="date"
-              id="date_to"
-              name="date_to"
-              placeholder="Enddatum eingeben"
-              autoComplete="off"
-              required
-              value={tripData.date_to}
-              onChange={handleInputChange}
-              className="w-full"
-            />
+          <div className="flex gap-2 w-full">
+            <div className="flex flex-col gap-2 w-full">
+              <RequiredLabel htmlFor="plz">PLZ</RequiredLabel>
+              <Input
+                type="text"
+                id="plz"
+                name="plz"
+                placeholder="PLZ eingeben"
+                autoComplete="off"
+                required
+                value={tripData.plz}
+                onChange={handleInputChange}
+                className="w-full"
+              />
+            </div>
+            <div className="flex flex-col gap-2 w-full">
+              <RequiredLabel htmlFor="ort">Ort</RequiredLabel>
+              <Input
+                type="text"
+                id="ort"
+                name="ort"
+                placeholder="Ort eingeben"
+                autoComplete="off"
+                required
+                value={tripData.ort}
+                onChange={handleInputChange}
+                className="w-full"
+              />
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-2 w-full">
-          <RequiredLabel htmlFor="land">Land</RequiredLabel>
+          <div className="flex gap-2 w-full">
+            <div className="flex flex-col gap-2 w-full">
+              <RequiredLabel htmlFor="street">Straße</RequiredLabel>
+              <Input
+                type="text"
+                id="street"
+                name="street"
+                placeholder="Straße eingeben"
+                autoComplete="off"
+                required
+                value={tripData.street}
+                onChange={handleInputChange}
+                className="w-full"
+              />
+            </div>
+            <div className="flex flex-col gap-2 w-full">
+              <RequiredLabel htmlFor="street_number">Hausnummer</RequiredLabel>
+              <Input
+                type="text"
+                id="street_number"
+                name="street_number"
+                placeholder="Hausnummer eingeben"
+                autoComplete="off"
+                required
+                value={tripData.street_number}
+                onChange={handleInputChange}
+                className="w-full"
+              />
+            </div>
+          </div>
+          <div className="flex gap-2 w-full">
+            <div className="flex flex-col gap-2 w-full">
+              <RequiredLabel htmlFor="rooms">Zimmer</RequiredLabel>
+              <Input
+                type="number"
+                id="rooms"
+                name="rooms"
+                placeholder="Zimmer eingeben"
+                autoComplete="off"
+                required
+                value={tripData.rooms}
+                onChange={handleInputChange}
+                className="w-full"
+              />
+            </div>
+            <div className="flex flex-col gap-2 w-full">
+              <RequiredLabel htmlFor="beds">Betten</RequiredLabel>
+              <Input
+                type="number"
+                id="beds"
+                name="beds"
+                placeholder="Bettplätze eingeben"
+                autoComplete="off"
+                required
+                value={tripData.beds}
+                onChange={handleInputChange}
+                className="w-full"
+              />
+            </div>
+          </div>
+          <RequiredLabel htmlFor="anreise_link">Anreise Link</RequiredLabel>
           <Input
             type="text"
-            id="land"
-            name="land"
-            placeholder="Land eingeben"
+            id="anreise_link"
+            name="anreise_link"
+            placeholder="https://maps.google.com/..."
             autoComplete="off"
             required
-            value={tripData.land}
+            value={tripData.anreise_link}
             onChange={handleInputChange}
             className="w-full"
           />
-          <Label htmlFor="area">Gebiet</Label>
-          <Input
-            type="text"
-            id="area"
-            name="area"
-            placeholder="Gebiet eingeben"
-            autoComplete="off"
-            value={tripData.area || ''}
-            onChange={handleInputChange}
-            className="w-full"
-          />
-        </div>
-        <div className="flex gap-2 w-full">
-          <div className="flex flex-col gap-2 w-full">
-            <RequiredLabel htmlFor="plz">PLZ</RequiredLabel>
-            <Input
-              type="text"
-              id="plz"
-              name="plz"
-              placeholder="PLZ eingeben"
-              autoComplete="off"
-              required
-              value={tripData.plz}
-              onChange={handleInputChange}
-              className="w-full"
-            />
-          </div>
-          <div className="flex flex-col gap-2 w-full">
-            <RequiredLabel htmlFor="ort">Ort</RequiredLabel>
-            <Input
-              type="text"
-              id="ort"
-              name="ort"
-              placeholder="Ort eingeben"
-              autoComplete="off"
-              required
-              value={tripData.ort}
-              onChange={handleInputChange}
-              className="w-full"
-            />
-          </div>
-        </div>
-        <div className="flex gap-2 w-full">
-          <div className="flex flex-col gap-2 w-full">
-            <RequiredLabel htmlFor="street">Straße</RequiredLabel>
-            <Input
-              type="text"
-              id="street"
-              name="street"
-              placeholder="Straße eingeben"
-              autoComplete="off"
-              required
-              value={tripData.street}
-              onChange={handleInputChange}
-              className="w-full"
-            />
-          </div>
-          <div className="flex flex-col gap-2 w-full">
-            <RequiredLabel htmlFor="street_number">Hausnummer</RequiredLabel>
-            <Input
-              type="text"
-              id="street_number"
-              name="street_number"
-              placeholder="Hausnummer eingeben"
-              autoComplete="off"
-              required
-              value={tripData.street_number}
-              onChange={handleInputChange}
-              className="w-full"
-            />
-          </div>
-        </div>
-        <div className="flex gap-2 w-full">
-          <div className="flex flex-col gap-2 w-full">
-            <RequiredLabel htmlFor="rooms">Zimmer</RequiredLabel>
+          <Label htmlFor="down_payment">Anzahlung</Label>
+          <div className="relative">
             <Input
               type="number"
-              id="rooms"
-              name="rooms"
-              placeholder="Zimmer eingeben"
+              id="down_payment"
+              name="down_payment"
+              placeholder="Anzahlung eingeben"
               autoComplete="off"
-              required
-              value={tripData.rooms}
+              value={tripData.down_payment ?? ''}
               onChange={handleInputChange}
-              className="w-full"
+              className="w-full pl-6"
             />
+            <span className="absolute left-2 top-1/2 transform -translate-y-1/2">€</span>
           </div>
-          <div className="flex flex-col gap-2 w-full">
-            <RequiredLabel htmlFor="beds">Betten</RequiredLabel>
+
+          <Label htmlFor="full_payment">Hauptzahlung</Label>
+          <div className="relative">
             <Input
               type="number"
-              id="beds"
-              name="beds"
-              placeholder="Bettplätze eingeben"
+              id="full_payment"
+              name="full_payment"
+              placeholder="Hauptzahlung eingeben"
               autoComplete="off"
-              required
-              value={tripData.beds}
+              value={tripData.full_payment ?? ''}
               onChange={handleInputChange}
-              className="w-full"
+              className="w-full pl-6"
             />
+            <span className="absolute left-2 top-1/2 transform -translate-y-1/2">€</span>
           </div>
-        </div>
-        <RequiredLabel htmlFor="anreise_link">Anreise Link</RequiredLabel>
-        <Input
-          type="text"
-          id="anreise_link"
-          name="anreise_link"
-          placeholder="https://maps.google.com/..."
-          autoComplete="off"
-          required
-          value={tripData.anreise_link}
-          onChange={handleInputChange}
-          className="w-full"
-        />
-        <Input type="hidden" id="group_id" name="group_id" value={tripData.group_id} readOnly />
-        <ResponsiveDialog
-          title="Trip erstellen"
-          message={`Bist du sicher, dass du das Trip erstellen möchtest?`}
-          confirmText="Trip erstellen"
-          onConfirm={handleCreateTrip}
-        >
-          <SubmitButton
-            aria-label="Reise erstellen"
-            pendingText="Erstelle Reise..."
-            className="flex text-xs pl-10 w-full"
-            disabled={!isFormValid}
+
+          <Label htmlFor="final_payment">Abschlussbetrag</Label>
+          <div className="relative">
+            <Input
+              type="number"
+              id="final_payment"
+              name="final_payment"
+              placeholder="Abschlussbetrag eingeben"
+              autoComplete="off"
+              value={tripData.final_payment ?? ''}
+              onChange={handleInputChange}
+              className="w-full pl-6"
+            />
+            <span className="absolute left-2 top-1/2 transform -translate-y-1/2">€</span>
+          </div>
+          <Input type="hidden" id="group_id" name="group_id" value={tripData.group_id} readOnly />
+          <ResponsiveDialog
+            title="Trip erstellen"
+            message={`Bist du sicher, dass du das Trip erstellen möchtest?`}
+            confirmText="Trip erstellen"
+            onConfirm={handleCreateTrip}
           >
-            <span className="xs:inline truncate">Reise erstellen</span>
-          </SubmitButton>
-        </ResponsiveDialog>
-      </div>
-    </form>
+            <SubmitButton
+              aria-label="Reise erstellen"
+              pendingText="Erstelle Reise..."
+              className="flex text-xs pl-10 w-full"
+              disabled={!isFormValid}
+            >
+              <span className="xs:inline truncate">Reise erstellen</span>
+            </SubmitButton>
+          </ResponsiveDialog>
+        </div>
+      </form>
+    </div>
   )
 }
