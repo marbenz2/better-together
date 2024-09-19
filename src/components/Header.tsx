4c935828dbd5092ebbde3de3210fa97d0652ffde
marbenz2 <benzinger.marcel@gmail.com>
2024-09-19 18:10:59 +0200

    Refactor Header component to improve readability and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,13 @@ export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <div className="flex gap-8 justify-center items-center">
-        <p className="uppercase font-bold">Better together.</p>
-        <h1 className="sr-only">
-          Better together. Ausflüge gemeinsam erleben.
-        </h1>
+        <p className="uppercase text-2xl font-black">Better together.</p>
       </div>
 
-      <p className="text-2xl lg:text-3xl font-thin !leading-tight mx-auto max-w-xl text-center">
-        Mache dir und deiner Familie oder deinen Freunden den gemeinsamen Urlaub
-        leichter.
+      <p className="text-2xl lg:text-3xl !leading-tight mx-auto max-w-xl text-center">
+        Mache dir und deinen Freunden den gemeinsamen Urlaub leichter.
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent" />
     </div>
-  );
+  )
 }
