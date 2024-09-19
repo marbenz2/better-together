18f912b4ed2872e3af771687d478153f36b152a8
marbenz2 <benzinger.marcel@gmail.com>
2024-09-19 19:31:35 +0200

    Refactor RenameGroup component to use ResponsiveDialog for group renaming confirmation

--- src/components/groups/RenameGroup.tsx
+++ src/components/groups/RenameGroup.tsx
@@ -8,16 +8,20 @@ import { SubmitButton } from '@/components/forms/submit-button'
 import { CardTitle } from '@/components/ui/card'
 import { FolderPenIcon } from 'lucide-react'
 import { useGroupStore } from '@/stores/groupStores'
+import { ResponsiveDialog } from '../ResponsiveDialog'
 
 export default function RenameGroup() {
   const { renameGroup, userGroups, groupId } = useGroupStore()
   const [changeGroupName, setChangeGroupName] = useState('')
+  const groupName = userGroups.find((group) => group.group_id === groupId)?.groups.name || ''
 
-  const handleRenameGroup = async (formData: FormData) => {
-    const groupId = formData.get('groupIdChange') as string
-    const newName = formData.get('newGroupName') as string
+  const handleRenameGroup = async () => {
     try {
-      await renameGroup(groupId, newName)
+      if (groupId) {
+        await renameGroup(groupId, changeGroupName)
+      } else {
+        console.error('Group ID is null or undefined')
+      }
       setChangeGroupName('')
     } catch (error) {
       console.error('Fehler beim Umbenennen der Gruppe:', error)
@@ -28,8 +32,12 @@ export default function RenameGroup() {
     setChangeGroupName(event.target.value)
   }
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }
+
   return (
-    <form className="flex flex-col gap-4 w-full">
+    <form className="flex flex-col gap-4 w-full" onSubmit={handleSubmit}>
       <Label htmlFor="newGroupName">
         <CardTitle className="text-xl">Gruppe umbenennen</CardTitle>
       </Label>
@@ -38,24 +46,30 @@ export default function RenameGroup() {
           type="text"
           id="newGroupName"
           name="newGroupName"
-          placeholder={userGroups.find((group) => group.group_id === groupId)?.groups.name || ''}
+          placeholder={groupName}
           autoComplete="off"
           required
           value={changeGroupName}
           onChange={handleGroupRenameInputChange}
           className="w-full max-w-lg"
         />
-        <input type="hidden" name="groupIdChange" value={groupId || ''} />
-        <SubmitButton
-          aria-label="Gruppe umbenennen"
-          formAction={handleRenameGroup}
-          pendingText="Umbenennen der Gruppe..."
-          className="relative flex text-xs pl-10 w-full max-w-lg"
+        <ResponsiveDialog
+          title="Gruppe umbenennen"
+          message={`Bist du sicher, dass du die Gruppe "${groupName}" in "${changeGroupName}" umbenennen möchtest?`}
+          confirmText="Gruppe umbenennen"
+          onConfirm={handleRenameGroup}
           disabled={changeGroupName === ''}
         >
-          <span className="xs:inline truncate">Gruppe umbenennen</span>
-          <FolderPenIcon className="absolute left-4 top-1/2 -translate-y-1/2 w-4 h-4" />
-        </SubmitButton>
+          <SubmitButton
+            aria-label="Gruppe umbenennen"
+            pendingText="Gruppe wird umbenannt..."
+            className="relative flex text-xs pl-10 w-full max-w-lg"
+            disabled={changeGroupName === ''}
+          >
+            <span className="xs:inline truncate">Gruppe umbenennen</span>
+            <FolderPenIcon className="absolute left-4 top-1/2 -translate-y-1/2 w-4 h-4" />
+          </SubmitButton>
+        </ResponsiveDialog>
       </div>
     </form>
   )
