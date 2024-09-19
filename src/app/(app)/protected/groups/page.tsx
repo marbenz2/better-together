c1a3474116e7b1bf1b8e6c9c9a67163644b5e5c7
marbenz2 <benzinger.marcel@gmail.com>
2024-09-19 18:10:10 +0200

    Refactor function name in groups page component

--- src/app/(app)/protected/groups/page.tsx
+++ src/app/(app)/protected/groups/page.tsx
@@ -5,7 +5,7 @@ import Groups from '@/components/groups/Groups'
 import { useToast } from '@/components/ui/use-toast'
 import { useToastStore } from '@/stores/toastStore'
 
-export default function Home() {
+export default function GroupsPage() {
   const { toast } = useToast()
   const { title, message, variant } = useToastStore()
 
