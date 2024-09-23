a3d7d7a8a9a62712937c9fffee9f230840d8b85a
marbenz2 <benzinger.marcel@gmail.com>
2024-09-23 21:26:37 +0200

    refactor: updated ProfilePage

--- src/app/(app)/protected/profile/page.tsx
+++ src/app/(app)/protected/profile/page.tsx
@@ -1,5 +1,23 @@
-import React from 'react'
+'use client'
+
+import { useEffect } from 'react'
+import { useToast } from '@/components/ui/use-toast'
+import { useToastStore } from '@/stores/toastStore'
+import Profile from '@/components/profile/Profile'
 
 export default function ProfilePage() {
-  return <div>PROFILEPAGE</div>
+  const { toast } = useToast()
+  const { title, message, variant } = useToastStore()
+
+  useEffect(() => {
+    if (title && message && variant) {
+      toast({
+        title,
+        description: message,
+        variant,
+      })
+    }
+  }, [title, message, variant, toast])
+
+  return <Profile />
 }
