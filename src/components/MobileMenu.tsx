a9df8d8832ce38f754665904f9992b1a94e48f2f
marbenz2 <benzinger.marcel@gmail.com>
2024-10-02 12:35:20 +0200

    refactor: new style for mobile menu and added social media links/icons

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -11,6 +11,8 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet'
+import { Separator } from './ui/separator'
+import { FacebookIcon, InstagramIcon, TwitterIcon } from 'lucide-react'
 
 const navLinks = [
   { href: '/protected', label: 'Übersicht' },
@@ -39,18 +41,39 @@ export default function MobileNavigation() {
     <Sheet open={isOpen} onOpenChange={handleOpenChange}>
       <SheetDescription hidden>Mobile Navigation</SheetDescription>
       <SheetTrigger asChild>
-        <MobileButton className="block md:hidden" />
+        <MobileButton className="block lg:hidden" />
       </SheetTrigger>
-      <SheetContent side={'top'} className="flex w-full justify-center">
+      <SheetContent side={'left'} className="flex flex-col h-full p-0">
         <SheetHeader>
           <SheetTitle></SheetTitle>
         </SheetHeader>
-        <div className="flex flex-col items-center gap-12 py-12">
-          {navLinks.map(({ href, label }) => (
-            <Link key={label} href={href} className="uppercase" onClick={handleLinkClick}>
-              {label}
+        <div className="flex flex-col justify-between w-full h-full py-10">
+          <div className="flex flex-col w-full items-start">
+            {navLinks.map(({ href, label }, index) => (
+              <div key={label} className="flex flex-col w-full">
+                <Link
+                  href={href}
+                  className="flex w-full px-6 py-6 items-center hover:bg-primary transition-colors duration-300"
+                  onClick={handleLinkClick}
+                >
+                  <span className="uppercase">{label}</span>
+                </Link>
+                {index < navLinks.length - 1 && <Separator className="bg-white/10" />}
+              </div>
+            ))}
+          </div>
+          {/* social media icons/link on the bottom of the menu centered */}
+          <div className="flex justify-center w-full gap-4 self-end">
+            <Link href={'https://www.instagram.com/'} target="_blank" rel="noopener noreferrer">
+              <InstagramIcon className="w-6 h-6 hover:text-primary transition-colors duration-300" />
             </Link>
-          ))}
+            <Link href={'https://www.x.com/'} target="_blank" rel="noopener noreferrer">
+              <TwitterIcon className="w-6 h-6 hover:text-primary transition-colors duration-300" />
+            </Link>
+            <Link href={'https://www.facebook.com/'} target="_blank" rel="noopener noreferrer">
+              <FacebookIcon className="w-6 h-6 hover:text-primary transition-colors duration-300" />
+            </Link>
+          </div>
         </div>
       </SheetContent>
     </Sheet>
