2e4d335b6dd448ef293b551f33810a001b5b38fe
marbenz2 <benzinger.marcel@gmail.com>
2024-10-02 12:34:07 +0200

    style: added a logo and changed the formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
+import { HeartHandshakeIcon } from 'lucide-react'
+
 export default function Header() {
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <p className="uppercase text-2xl font-black">Better together.</p>
-      </div>
-
-      <p className="text-2xl lg:text-3xl !leading-tight mx-auto max-w-xl text-center">
+    <div className="flex flex-col gap-8 items-center">
+      <HeartHandshakeIcon className="w-28 h-28 text-[#c940ce]" strokeWidth={1} />
+      <h2 className="!leading-tight mx-auto max-w-xl text-center">
         Mache dir und deinen Freunden den gemeinsamen Urlaub leichter.
-      </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent" />
+      </h2>
+      <div className="flex gap-8 justify-center items-center">
+        <h1 className="uppercase text-3xl font-black">Better together.</h1>
+      </div>
     </div>
   )
 }
