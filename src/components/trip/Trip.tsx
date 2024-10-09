430b9fc40101b3918d6ea48771a7e08f47838fae
marbenz2 <benzinger.marcel@gmail.com>
2024-10-09 18:57:29 +0200

    feat: Add component for adding additional trip members

--- src/components/trip/Trip.tsx
+++ src/components/trip/Trip.tsx
@@ -1,6 +1,12 @@
 'use client'
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  CardBackPlate,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import Image from 'next/image'
 import { PencilIcon, SquareArrowOutUpRight, Trash2Icon, UsersIcon } from 'lucide-react'
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@/components/ui/table'
@@ -20,7 +26,7 @@ import { Button } from '../ui/button'
 export default function Trip() {
   const router = useRouter()
   const { user, subscribedTrips, isSubscribed, setIsSubscribed } = useUserStore()
-  const { tripMembers, trip, deleteTrip, getTripMembers } = useTripStore()
+  const { tripMembers, trip, deleteTrip, getTripMembers, availableSpots } = useTripStore()
   const { tripPublicProfiles, userGroups, getAllTripPublicProfiles } = useGroupStore()
 
   const [isLoading, setIsLoading] = useState(false)
@@ -95,92 +101,90 @@ export default function Trip() {
   if (isLoading) return <Spinner />
 
   return (
-    <>
-      <Card className="flex flex-col md:flex-row max-w-7xl w-full">
-        <CardHeader className="flex-1">
-          <div className="flex justify-between mb-4">
-            <BackButtonClient className="static" />
-            {isCreator && (
-              <div className="flex gap-4">
-                <ButtonLink href={`/protected/trips/${trip.id}/user-list`}>
-                  <UsersIcon className="w-4 h-4" />
-                </ButtonLink>
-                <ResponsiveDialog
-                  title="Reise bearbeiten"
-                  message="Wollen Sie diese Reise wirklich bearbeiten?"
-                  confirmText="Reise bearbeiten"
-                  onConfirm={handleEditTrip}
-                >
-                  <Button>
-                    <PencilIcon className="w-4 h-4" />
-                  </Button>
-                </ResponsiveDialog>
-                <ResponsiveDialog
-                  title="Reise löschen"
-                  message="Wollen Sie diese Reise wirklich löschen?"
-                  confirmText="Reise löschen"
-                  info="Diese Aktion kann nicht rückgängig gemacht werden."
-                  infoType="warning"
-                  buttonVariant="destructive"
-                  onConfirm={handleDeleteTrip}
-                >
-                  <Button variant="destructive">
-                    <Trash2Icon className="w-4 h-4" />
-                  </Button>
-                </ResponsiveDialog>
-              </div>
-            )}
-          </div>
-          <div className="relative flex w-full h-64 md:h-full">
-            <Image
-              loading="lazy"
-              src={trip.image}
-              alt={trip.name}
-              fill
-              className="object-contain object-top"
-            />
-          </div>
-        </CardHeader>
-        <CardContent className="flex flex-col md:pt-6 md:pl-0 gap-12 justify-between flex-1">
-          <div className="flex flex-col gap-4">
-            <CardHeader className="p-0">
-              <CardTitle>{trip.name}</CardTitle>
-              <CardDescription>
-                {new Date(trip.date_from).toLocaleDateString('de-DE', {
-                  weekday: 'short',
-                  year: 'numeric',
-                  month: 'long',
-                  day: 'numeric',
-                })}{' '}
-                -
-                <br />
-                {new Date(trip.date_to).toLocaleDateString('de-DE', {
-                  weekday: 'short',
-                  year: 'numeric',
-                  month: 'long',
-                  day: 'numeric',
-                })}
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="p-0">
-              Euer nächstes Abenteuer erwartet euch in{' '}
-              <span className="font-bold">{trip.land}</span>! Diese Reise führt euch nach{' '}
-              <span className="font-bold">{trip.ort}</span>
-              {trip.ort !== trip.area && (
-                <>
-                  , einem charmanten Ort in der malerischen Region{' '}
-                  <span className="font-bold">{trip.area}</span>
-                </>
-              )}
-              . Eure Unterkunft findet ihr in der{' '}
-              <span className="font-bold">
-                {trip.street} {trip.street_number}
-              </span>
-              . Bereitet euch auf eine unvergessliche Zeit vor, voller spannender Erlebnisse und
-              entspannender Momente! Um eure Anreise so einfach wie möglich zu gestalten, könnt ihr
-              euch hier direkt die Route anzeigen lassen: <br />
+    <CardBackPlate className="flex flex-col md:flex-row max-w-7xl w-full">
+      <CardHeader className="flex-1">
+        <div className="flex justify-between mb-4">
+          <BackButtonClient className="static" />
+          {isCreator && (
+            <div className="flex gap-4">
+              <ButtonLink href={`/protected/trips/${trip.id}/user-list`}>
+                <UsersIcon className="w-4 h-4" />
+              </ButtonLink>
+              <ResponsiveDialog
+                title="Reise bearbeiten"
+                message="Wollen Sie diese Reise wirklich bearbeiten?"
+                confirmText="Reise bearbeiten"
+                onConfirm={handleEditTrip}
+              >
+                <Button>
+                  <PencilIcon className="w-4 h-4" />
+                </Button>
+              </ResponsiveDialog>
+              <ResponsiveDialog
+                title="Reise löschen"
+                message="Wollen Sie diese Reise wirklich löschen?"
+                confirmText="Reise löschen"
+                info="Diese Aktion kann nicht rückgängig gemacht werden."
+                infoType="warning"
+                buttonVariant="destructive"
+                onConfirm={handleDeleteTrip}
+              >
+                <Button variant="destructive">
+                  <Trash2Icon className="w-4 h-4" />
+                </Button>
+              </ResponsiveDialog>
+            </div>
+          )}
+        </div>
+        <div className="relative flex w-full h-64 md:h-full">
+          <Image
+            loading="lazy"
+            src={trip.image}
+            alt={trip.name}
+            fill
+            className="object-contain object-top"
+          />
+        </div>
+      </CardHeader>
+      <CardContent className="flex flex-col md:pt-6 md:pl-0 gap-12 justify-between flex-1">
+        <div className="flex flex-col gap-4">
+          <CardHeader className="p-0">
+            <CardTitle>{trip.name}</CardTitle>
+            <CardDescription>
+              {new Date(trip.date_from).toLocaleDateString('de-DE', {
+                weekday: 'short',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+              })}{' '}
+              -
               <br />
-              {/*               <Link
+              {new Date(trip.date_to).toLocaleDateString('de-DE', {
+                weekday: 'short',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+              })}
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="p-0">
+            Euer nächstes Abenteuer erwartet euch in <span className="font-bold">{trip.land}</span>!
+            Diese Reise führt euch nach <span className="font-bold">{trip.ort}</span>
+            {trip.ort !== trip.area && (
+              <>
+                , einem charmanten Ort in der malerischen Region{' '}
+                <span className="font-bold">{trip.area}</span>
+              </>
+            )}
+            . Eure Unterkunft findet ihr in der{' '}
+            <span className="font-bold">
+              {trip.street} {trip.street_number}
+            </span>
+            . Bereitet euch auf eine unvergessliche Zeit vor, voller spannender Erlebnisse und
+            entspannender Momente! Um eure Anreise so einfach wie möglich zu gestalten, könnt ihr
+            euch hier direkt die Route anzeigen lassen: <br />
+            <br />
+            {/*               <Link
                 href={trip.anreise_link}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -188,99 +192,109 @@ export default function Trip() {
               >
                 Google Maps <SquareArrowOutUpRight className="w-4 h-4" />
               </Link> */}
-              <ButtonLink
-                href={trip.anreise_link}
-                label="Google Maps"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="w-fit"
-              >
-                <SquareArrowOutUpRight className="w-4 h-4" />
-              </ButtonLink>
-            </CardContent>
-          </div>
-          <Table>
-            <TableBody>
+            <ButtonLink
+              href={trip.anreise_link}
+              label="Google Maps"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-fit"
+            >
+              <SquareArrowOutUpRight className="w-4 h-4" />
+            </ButtonLink>
+          </CardContent>
+        </div>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableHead>Location:</TableHead>
+              <TableCell>{trip.location_name}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHead>Land:</TableHead>
+              <TableCell>{trip.land}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHead>Gebiet:</TableHead>
+              <TableCell>{trip.area || '-'}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHead>Ort:</TableHead>
+              <TableCell>
+                {trip.plz} {trip.ort}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHead>Anschrift:</TableHead>
+              <TableCell>
+                {trip.street} {trip.street_number}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHead>Zimmer</TableHead>
+              <TableCell>{trip.rooms}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHead>Betten</TableHead>
+              <TableCell>{trip.beds}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHead>Verfügbare Plätze</TableHead>
+              <TableCell>
+                {availableSpots} / {trip.max_spots}
+              </TableCell>
+            </TableRow>
+            {tripMembers && (
               <TableRow>
-                <TableHead>Location:</TableHead>
-                <TableCell>{trip.location_name}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableHead>Land:</TableHead>
-                <TableCell>{trip.land}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableHead>Gebiet:</TableHead>
-                <TableCell>{trip.area || '-'}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableHead>Ort:</TableHead>
+                <TableHead>Teilnehmer</TableHead>
                 <TableCell>
-                  {trip.plz} {trip.ort}
+                  {tripPublicProfiles.length === 0 && '-'}
+                  {tripPublicProfiles.map((profile, index) => (
+                    <span key={profile.id}>
+                      {profile.first_name} {profile.last_name}
+                      {index < tripPublicProfiles.length - 1 && ', '}
+                    </span>
+                  ))}
                 </TableCell>
               </TableRow>
+            )}
+            {trip.initial_down_payment && trip.initial_down_payment > 0 ? (
               <TableRow>
-                <TableHead>Anschrift:</TableHead>
+                <TableHead>Anzahlung</TableHead>
                 <TableCell>
-                  {trip.street} {trip.street_number}
+                  {Intl.NumberFormat('de-DE', {
+                    style: 'currency',
+                    currency: 'EUR',
+                  }).format(trip.initial_down_payment)}
                 </TableCell>
               </TableRow>
-              <TableRow>
-                <TableHead>Zimmer</TableHead>
-                <TableCell>{trip.rooms}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableHead>Betten</TableHead>
-                <TableCell>{trip.beds}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableHead>Verfügbare Plätze</TableHead>
-                <TableCell>
-                  {trip.available_spots} / {trip.max_spots}
-                </TableCell>
-              </TableRow>
-              {tripMembers && (
-                <TableRow>
-                  <TableHead>Teilnehmer</TableHead>
-                  <TableCell>
-                    {tripPublicProfiles.length === 0 && '-'}
-                    {tripPublicProfiles.map((profile, index) => (
-                      <span key={profile.id}>
-                        {profile.first_name} {profile.last_name}
-                        {index < tripPublicProfiles.length - 1 && ', '}
-                      </span>
-                    ))}
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>{' '}
-          {isSubscribed && timeOfSubscription && (
-            <CardDescription className="text-muted-foreground">
-              Angemeldet am:{' '}
-              {new Date(timeOfSubscription).toLocaleDateString('de-DE', {
-                weekday: 'short',
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-              })}
-            </CardDescription>
-          )}
-          {!tripDone && isSubscribed && trip.available_spots > 0 && <TripSubscription />}
-          {!tripDone && !isSubscribed && trip.available_spots > 0 && <TripSubscription />}
-          {!tripDone && isSubscribed && trip.available_spots === 0 && <TripSubscription />}
-          {!tripDone && !isSubscribed && trip.available_spots === 0 && (
-            <CardDescription className="text-muted-foreground">
-              Diese Reise ist bereits ausgebucht.
-            </CardDescription>
-          )}
-          {tripDone && (
-            <CardDescription className="text-muted-foreground">
-              Diese Reise ist bereits vorbei. Wir hoffen, ihr hattet eine tolle Zeit!
-            </CardDescription>
-          )}
-        </CardContent>
-      </Card>
-    </>
+            ) : null}
+          </TableBody>
+        </Table>{' '}
+        {isSubscribed && timeOfSubscription && (
+          <CardDescription className="text-muted-foreground">
+            Angemeldet am:{' '}
+            {new Date(timeOfSubscription).toLocaleDateString('de-DE', {
+              weekday: 'short',
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+          </CardDescription>
+        )}
+        {!tripDone && isSubscribed && trip.available_spots > 0 && <TripSubscription />}
+        {!tripDone && !isSubscribed && trip.available_spots > 0 && <TripSubscription />}
+        {!tripDone && isSubscribed && trip.available_spots === 0 && <TripSubscription />}
+        {!tripDone && !isSubscribed && trip.available_spots === 0 && (
+          <CardDescription className="text-muted-foreground">
+            Diese Reise ist bereits ausgebucht.
+          </CardDescription>
+        )}
+        {tripDone && (
+          <CardDescription className="text-muted-foreground">
+            Diese Reise ist bereits vorbei. Wir hoffen, ihr hattet eine tolle Zeit!
+          </CardDescription>
+        )}
+      </CardContent>
+    </CardBackPlate>
   )
 }
