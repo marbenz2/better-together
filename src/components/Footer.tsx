c7e1844186fe1300da96d4e5871809133fd5537a
marbenz2 <benzinger.marcel@gmail.com>
2024-10-10 09:33:12 +0200

    feat: added links for legal components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,25 @@
+import Link from 'next/link'
+
 export default function Footer() {
   return (
-    <footer className="w-full pt-8 pb-4 flex justify-center text-center text-xs">
+    <footer className="w-full pt-8 pb-4 flex flex-col justify-center text-center gap-2 text-xs">
       &copy; {new Date().getFullYear()} - MarBenz
+      <div>
+        <Link
+          href={'/data-protection'}
+          className="hover:text-primary transition-colors duration-300"
+        >
+          Datenschutz
+        </Link>
+        <span className="mx-2">|</span>
+        <Link href={'/imprint'} className="hover:text-primary transition-colors duration-300">
+          Impressum
+        </Link>
+        <span className="mx-2">|</span>
+        <Link href={'/terms-of-use'} className="hover:text-primary transition-colors duration-300">
+          Nutzungsbedingungen
+        </Link>
+      </div>
     </footer>
   )
 }
